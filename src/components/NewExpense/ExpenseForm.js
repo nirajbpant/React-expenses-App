8dc98b38c3f9b1b42bac30418b6559d9b104296c
Niraj Ballav Pant <nirajbpant@gmail.com>
2021-04-18 19:17:45 +0545

    Added filter optimization and condition returns

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -76,6 +76,7 @@ const ExpenseForm= (props)=>{
                     <input type="date" min="2019-01-01" max="2022-12-31" value={enteredDate}onChange={dateChangeHandler}/>
                 </div>
                 <div className="new-expense__actions">
+                    <button>Cancel</button>
                     <button type="submit">Add Expense</button>
                 </div>
             </div>
