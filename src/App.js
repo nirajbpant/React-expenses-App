d90ebba88e36a5f693a543a1aca7016d7d843070
Niraj Ballav Pant <nirajbpant@gmail.com>
2021-04-18 14:10:11 +0545

    Added useState hook and added form to add new expense

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Expenses from './components/Expenses';
-
-function App() {
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
+const App = ()=> {
 
   const expenses = [
     {
@@ -25,7 +25,7 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+     <NewExpense/>
       <Expenses items= {expenses} />
     </div>
   );
